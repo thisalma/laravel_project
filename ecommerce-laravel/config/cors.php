b29d7852d22e042979b1a4d7d4442c1ee76b682e
thisalma <alwisthisalma@gamil/.com>
2025-09-11 13:36:13 +0530

    trying to make the backend with the MAD assingmnet

---
+++ ecommerce-laravel/config/cors.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => ['http://localhost:8000', 'http://127.0.0.1:8000', 'http://192.168.1.6:8000'], // replace with your PC IP
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => true,
+
+];
